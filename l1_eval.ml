97175388bb1f825011f9438669ce67a2d6659437
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-11-02 21:35:49 +0000

    adding some steps

--- l1_eval.ml
+++ l1_eval.ml
@@ -36,23 +36,41 @@ type expr =
     |   Tl of expr
     |   Raise
     |   Try of expr * expr
-          
-          
-(* Implementacao da funcao STEP de avaliacao em um passo *)
-let rec step t = match t with
-    (* CASO IF ( t1 , t2 , t3 ) *)
-        If (TmTrue, t2, t3) −> t2 (* regra E−IfTrue *)
-    |   If (TmFalse, t2, t3) −> t3 (* regra E−False *)
-    |   If (t1, t2, t3) −> (* regra E−If *)
+        
+type result =
+        Vnum of int 
+    |   Vbool of bool 
+    |   Vpair of result * result
+    |   Vnil
+    |   Vcons of result * result 
+    |   Vclos of variable * expr * env
+    |   Vrclos of variable * variable * expr * env
+    |   RRaise
+and
+    env = (variable * result) list
+
+(* Excecao a ser ativada quando termo for uma FORMA NORMAL *)
+exception NoRuleApplies
+
+(* BIG STEP L1 *)
+let rec step env e = 
+    match (env, e) with
     
-        let t1' = step t1 in
-            If (t1', t2, t3) -> 
-            (* CASO SUCC ( t1 ) *)
+    |   Ncte(n) -> Vnum(n)
+    |   Bcte(b) -> Vbool(b)
+    |   Nil -> Vnil
+    |   Pair(e1, e2) -> Vpair(e1, e2)
+    |   If (e1, e2, e3) −> 
+    |   If (TmFalse, t2, t3) −> t3
+    |   If (t1, t2, t3) −>
+            let t1' = step (env, t1) in If (t1', t2, t3)
+    |   Binop (e
             
             
 (* Implementacao de EVAL *)
-let rec eval t =
-    try let t' = step t
-        in eval t'
+let rec eval (env, t) =
+    try let t' = step (env, t)
+        in eval (env, t')
     with NoRuleApplies −> t
     
+(* Tests *)
