25a55b8955defb9781fdf07bd67cf8aabb141c14
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-11-22 12:10:42 -0200

    fixing compilation problems on unify
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- build_all.bat
+++ build_all.bat
@@ -1,4 +1,4 @@
 mkdir -p _build
-sanitize.bat
 ocamlopt -I tests/ -o _build/test_collect typenames.ml collect.ml eval.ml main.ml tests/testexpressions.ml tests/test_collect.ml
 ocamlopt -I tests/ -o _build/test_eval typenames.ml collect.ml eval.ml main.ml tests/testexpressions.ml tests/test_eval.ml
+sanitize.bat
--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,7 @@
 OC_FLAGS="-I tests/"
-SOURCES="typenames.ml collect.ml eval.ml main.ml"
+SOURCES="typenames.ml collect.ml eval.ml unify.ml main.ml"
 mkdir -p _build
-sh sanitize.sh
 ocamlopt $OC_FLAGS -o _build/test_collect $SOURCES tests/testexpressions.ml tests/test_collect.ml
 ocamlopt $OC_FLAGS -o _build/test_eval $SOURCES tests/testexpressions.ml tests/test_eval.ml
+ocamlopt $OC_FLAGS -o _build/test_typeinfer $SOURCES
+sh sanitize.sh
