e56f9b8b5d772113e75b21e5e9662170f492f5f1
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-11-22 12:11:58 -0200

    fixing compilation problems on unify
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- build_all.bat
+++ build_all.bat
@@ -1,4 +1,5 @@
 mkdir -p _build
 ocamlopt -I tests/ -o _build/test_collect typenames.ml collect.ml eval.ml main.ml tests/testexpressions.ml tests/test_collect.ml
 ocamlopt -I tests/ -o _build/test_eval typenames.ml collect.ml eval.ml main.ml tests/testexpressions.ml tests/test_eval.ml
+ocamlopt -I tests/ -o _build/test_typeinfer typenames.ml collect.ml eval.ml unify.ml main.ml
 sanitize.bat
